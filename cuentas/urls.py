8cb239d2c3d0e90c516b8a30b3733ffe63f6691c
Matías Donoso <matiasdonoso135@gmail.com>
2025-12-12 01:30:30 -0300

    no lo se que hago a esta hora

--- cuentas/urls.py
+++ cuentas/urls.py
@@ -1,6 +1,34 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
+
 
 urlpatterns = [
+    
     path("crear-usuario/", views.crear_usuario, name="crear_usuario"),
+    # Logout tuyo
+    path("logout/", views.logout_view, name="logout"),
+
+    # ---- Recuperación de contraseña ----
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html"),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"),
+        name="password_reset_complete",
+    ),
 ]
